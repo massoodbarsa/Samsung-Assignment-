8d9c37537c48ab05a3fe31b3b52b14fe9c11a8a6
Amir Shakiba Nasab <amir.shakiba1980@gmail.com>
2021-07-07 01:41:55 +0200

    trim

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,14 +9,7 @@ export default function Navbar() {
         <div className='navigation'>
                 <div className='navigation__items'>
                     <ul>
-                        {/* <li><NavLink to='/dashboard'><Button variant="outlined" color="primary">Dashboard</Button></NavLink></li> */}
                         <li><NavLink to='/'><Button variant="outlined" color="primary">Home</Button></NavLink></li>
-
-                        {/* {context.token && <li><NavLink to='/bookings'><Button variant="outlined" color="primary">Bookings</Button></NavLink></li>} */}
-                        {/* {context.token && <li><button onClick={context.logout}  >Sign Out</button></li>} */}
-                        {/* {context.email && <li>
-                                <Button variant="outlined" color="primary" onClick={context.logout}>Sign Out</Button></li>} */}
-
                     </ul>
                 </div>
         </div>
