d844d7781325e0825ab7494cb4dd489fa5005768
Amir Shakiba Nasab <amir.shakiba1980@gmail.com>
2021-07-07 01:49:19 +0200

    add samsung logo to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,11 +7,14 @@ import { Button } from '@material-ui/core';
 export default function Navbar() {
     return (
         <div className='navigation'>
-                <div className='navigation__items'>
-                    <ul>
-                        <li><NavLink to='/'><Button variant="outlined" color="primary">Home</Button></NavLink></li>
-                    </ul>
-                </div>
+            <div className='navigation__logo'>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/2/24/Samsung_Logo.svg' alt="React Logo" />
+            </div>
+            <div className='navigation__items'>
+                <ul>
+                    <li><NavLink to='/'><Button variant="outlined" color="primary">Home</Button></NavLink></li>
+                </ul>
+            </div>
         </div>
     )
 }
