0c0d257af052192d12410fe1ad400992622d3720
Amir Shakiba Nasab <amir.shakiba1980@gmail.com>
2021-07-08 01:38:04 +0200

    make samsung logo Navlink to Home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,9 @@ export default function Navbar() {
     return (
         <div className='navigation'>
             <div className='navigation__logo'>
-                <img src='https://upload.wikimedia.org/wikipedia/commons/2/24/Samsung_Logo.svg' alt="React Logo" />
+                <NavLink to='/'>
+                    <img src='https://upload.wikimedia.org/wikipedia/commons/2/24/Samsung_Logo.svg' alt="React Logo" />
+                </NavLink>
             </div>
             <div className='navigation__items'>
                 <ul>
